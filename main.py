073ddced403a6ae88407a500ca8310fa567a8d83
Ayu <y.abouseada16@gmail.com>
2021-04-08 16:44:40 +0200

    v0.5

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #Lists
-li = [1,2,3,4]
+li = [1,2,3,4,4,4,5,6,6,6]
 li2= ['a','b','c','d']
 li3 = [1,2,'a',True]
 amazon_cart = ['notebooks','sunglasses','videogames','toys','grapes']
@@ -59,17 +59,56 @@ dictionary = {
     'b': 2
 }
 print(dictionary['b'])
-my_list = [
-{
+my_list =[
+    {
     'a': [1,2,3],
     'b': ['a','b','c'],
     'c': [6,7,8]
-},
-{
+}, {
     'a': [1,2,3],
     'b': ['a','h','t'],
     'c': [9,0,4]
-}
-    ]
+ }
+]
+
+print(my_list[0].get('o'))
 print(my_list[1]['b'][2])
-print(my_list.get('a'))
+print('g' in my_list[0])
+user = dict(name = 'Jhon')
+print(user)
+user_2={
+    'basket':[1,2,3],
+    'greet': 'hello',
+    'age' : 20
+}
+print('hello' in user_2.values())
+print('age' in user_2.keys())
+print(user_2.items())
+print(user_2.update({'age': 55}))
+print(user_2)
+# Tuple ---> list that doesn't change
+my_tuple = (1,2,3,4,5,6)
+# my_tuple[1]='z'
+##print(int(input('write what u want')) in my_tuple)
+print(my_tuple.count(2))
+print(my_tuple.index(2))
+print(len(my_tuple))
+#Set
+my_set = {1,2,3,4,5}
+my_set2={6,2,4,5,0}
+# my_set.add(100)
+# my_set.add(2)
+# print(my_set)
+# print(set(li))
+# print(my_set.difference(my_set2))
+# my_set.discard(2)
+# print(my_set)
+# my_set.difference_update(my_set2)
+# print(my_set)
+# print(my_set.intersection(my_set2))
+#print(my_set & my_set2)
+# print(my_set.isdisjoint(my_set2))
+# print(my_set.union(my_set2))
+# print(my_set | my_set2)
+#issubset
+#issuperset
